a50089f321481bba1e9b93948b2adb81e64ff087
sh-sekigawa <sh-sekigawa@kddi-research.jp>
2020-06-16 14:24:56 +0900

    Add output filename.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import argparse
 import json
 
 parse = argparse.ArgumentParser()
-parse.add_argument("filename", help='YAML filename.')
+parse.add_argument("inputfile", help='Input YAML filename.')
+parse.add_argument("outputfile", help='Output YAML filename.')
 parse.add_argument('-p', '--parameter', help='Input parameters as JSON format.', action='append')
 
 args = parse.parse_args()
@@ -14,17 +15,19 @@ if args.parameter is None:
     exit(-1)
 
 try:
-    f = open(args.filename, 'r')
+    f = open(args.inputfile, 'r')
     data = yaml.load(f, Loader=yaml.SafeLoader)
     for param in args.parameter:
         try:
             json_obj = json.loads(param)
             data.update(json_obj)
-            print(yaml.dump(data))
         except json.JSONDecodeError:
             print("Parameters in invalid format.", file=sys.stderr)
             exit(-1)
 
+    f.close()
+    f = open(args.outputfile,'w')
+    yaml.dump(data, f, encoding='utf-8', allow_unicode= True)
     f.close()
 except FileNotFoundError:
     print("File not found.", file=sys.stderr)
